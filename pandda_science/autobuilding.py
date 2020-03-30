b610b00a652235e976196de99a289101a10cea32
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2020-03-30 12:39:32 +0100

    Update autobuilding.py

--- pandda_science/autobuilding.py
+++ pandda_science/autobuilding.py
@@ -140,7 +140,7 @@ def get_autobuilding_rmsd_distribution_graph(autobuilding_rmsd_df: pd.DataFrame,
 
         # Comparitive log
         comparitive_cdf_plot({"phenix_control": np.log(cutoff_df["phenix_control_rmsd"] + 1),
-                              "phenix_event": np.log(cutoff_df["phenix_control_rmsd"] + 1),
+                              "phenix_event": np.log(cutoff_df["phenix_event_rmsd"] + 1),
                               },
                              output_path / "comparitive_rmsd_log_cdf_{}.png".format(cutoff),
                              )
