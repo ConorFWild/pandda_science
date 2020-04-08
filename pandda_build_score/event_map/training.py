78a34da302015c491f034d321d5ae43c4a01693c
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2020-04-08 16:25:17 +0100

    Update training.py

--- pandda_build_score/event_map/training.py
+++ pandda_build_score/event_map/training.py
@@ -88,7 +88,7 @@ def train(network,
 
                     recent_labels = labels[-999:]
 
-                    for cutoff in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.85, 0.9, 0.95]:
+                    for cutoff in [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.85, 0.9, 0.95]:
                         true_positives = 0
                         false_positives = 0
                         true_negatives = 0
