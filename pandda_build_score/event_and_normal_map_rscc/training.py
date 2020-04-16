78da4c3f5aaf16e92bb7cb79689851ebc646ab8a
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2020-04-16 17:50:59 +0100

    Update training.py

--- pandda_build_score/event_and_normal_map_rscc/training.py
+++ pandda_build_score/event_and_normal_map_rscc/training.py
@@ -32,10 +32,6 @@ def train(network,
     for epoch in range(epochs):
 
         labels = []
-        running_loss_30 = 0
-        running_loss_100 = 0
-        running_loss_300 = 0
-        running_loss_1000 = 0
 
         running_loss_rscc_30 = 0
         running_loss_rscc_100 = 0
@@ -48,41 +44,22 @@ def train(network,
             id_batch = batch["id"]
             rscc = batch["rscc"].type(torch.FloatTensor)
 
-            # print(sample_batch.shape)
-
             optimizer.zero_grad()
 
             sample_batch_cuda = sample_batch.cuda()
-            label_batch_cuda = label_batch.cuda()
             rscc_batch_cuda = rscc.cuda()
 
             estimated_label_batch_cuda, estimated_rscc_cuda = network(sample_batch_cuda)
-            # loss = loss_function(estimated_label_batch_cuda,
-            #                      label_batch_cuda,
-            #                      )
+
             loss = loss_function_rscc(estimated_rscc_cuda,
                                            rscc_batch_cuda,
                                            )
-            # print(estimated_rscc_cuda.type())
-            # print(rscc.type())
 
-            # total_loss = loss + loss_rscc
 
-            estimated_label_batch = estimated_label_batch_cuda.cpu()
             estimated_rscc = estimated_rscc_cuda.cpu()
             loss.backward()
             optimizer.step()
-            #
-            # loss_rscc.backward()
-            # optimizer.step()
 
-            # total_loss.backward()
-            # optimizer.step()
-
-            running_loss_30 += loss.item()
-            running_loss_100 += loss.item()
-            running_loss_300 += loss.item()
-            running_loss_1000 += loss.item()
 
             running_loss_rscc_30 += loss.item()
             running_loss_rscc_100 += loss.item()
@@ -91,98 +68,44 @@ def train(network,
 
             if i_batch % 30 == 29:  # print every 30 mini-batches
                 print("=30 dataset average=")
-                print("\tLoss at epoch {}, iteration {} is {}".format(epoch,
-                                                                      i_batch,
-                                                                      running_loss_30 / 30))
                 print("\tLoss rscc at epoch {}, iteration {} is {}".format(epoch,
                                                                       i_batch,
                                                                       running_loss_rscc_30 / 30))
                 print("\t{}".format(id_batch))
-                print("\t{}".format(estimated_label_batch))
                 print("\t{}".format(label_batch))
                 print("\t{}".format(rscc))
                 print("\t{}".format(estimated_rscc))
-                running_loss_30 = 0
                 running_loss_rscc_30 = 0
 
             if i_batch % 100 == 99:  # print every 30 mini-batches
                 print("===100 dataset average===")
-                print("\tLoss at epoch {}, iteration {} is {}".format(epoch,
-                                                                      i_batch,
-                                                                      running_loss_100 / 100))
+
                 print("\tLoss rscc at epoch {}, iteration {} is {}".format(epoch,
                                                                       i_batch,
                                                                       running_loss_rscc_100 / 100))
-                running_loss_100 = 0
                 running_loss_rscc_100 = 0
 
 
             if i_batch % 300 == 299:  # print every 30 mini-batches
                 print("=====300 dataset average=====")
-                print("\tLoss at epoch {}, iteration {} is {}".format(epoch,
-                                                                      i_batch,
-                                                                      running_loss_300 / 300))
+
                 print("\tLoss rscc at epoch {}, iteration {} is {}".format(epoch,
                                                                       i_batch,
                                                                       running_loss_rscc_300 / 300))
-                running_loss_300 = 0
                 running_loss_rscc_300 = 0
 
 
             if i_batch % 1000 == 999:  # print every 30 mini-batches
                 print("=======1000 dataset average=======")
-                print("\tLoss at epoch {}, iteration {} is {}".format(epoch,
-                                                                      i_batch,
-                                                                      running_loss_1000 / 1000))
+
                 print("\tLoss rscc at epoch {}, iteration {} is {}".format(epoch,
                                                                       i_batch,
                                                                       running_loss_rscc_1000 / 1000))
-                running_loss_1000 = 0
                 running_loss_rscc_3000 = 0
 
 
                 recent_labels = labels[-999:]
 
-                for cutoff in [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.85, 0.9, 0.95]:
-                    true_positives = 0
-                    false_positives = 0
-                    true_negatives = 0
-                    false_negatives = 0
-
-                    for label in recent_labels:
-                        if label["estimated_class"] >= cutoff:
-                            if label["true_class"] == 1:
-                                true_positives += 1
-                            else:
-                                false_positives += 1
-                        else:
-                            if label["true_class"] == 1:
-                                false_negatives += 1
-                            else:
-                                true_negatives += 1
-                    print("\tAt cutoff: {}".format(cutoff))
-                    print("\t\tThe number of true positives is: {}".format(true_positives))
-                    print("\t\tThe number of false positives is: {}".format(false_positives))
-                    print("\t\tThe number of false negatives is: {}".format(false_negatives))
-                    print("\t\tThe number of true Negatives is: {}".format(true_negatives))
-                    if true_positives+false_positives > 0:
-                        print("\t\tPrecision is: {}".format(true_positives/(true_positives+false_positives)))
-                    else:
-                        print("\t\tPrecision is: {}".format(0))
-                    if true_positives+false_negatives > 0:
-                        print("\t\tRecall is: {}".format(true_positives/(true_positives+false_negatives)))
-                    else:
-                        print("\t\tRecall is: {}".format(0))
-
-                table = pd.DataFrame(recent_labels)
-                table = table[table["true_class"] == 0]
-
-                print([(row["pandda_name"],
-                        row["dtag"],
-                        row["event_idx"],
-                        row["estimated_class"],
-                        row["coords"],
-                        row["event_map_path"],) for index, row in table.sort_values(by="estimated_class").tail(10).iterrows()])
 
             for i, index in enumerate(id_batch["pandda_name"]):
                 pandda_name = deepcopy(id_batch["pandda_name"][i])
@@ -190,15 +113,12 @@ def train(network,
                 event_idx = deepcopy(id_batch["event_idx"][i].detach().numpy())
                 class_array = deepcopy(label_batch[i].detach().numpy())
                 true_class = np.argmax(class_array)
-                estimated_class_array = deepcopy(estimated_label_batch[i].detach().numpy())
-                estimated_class = estimated_class_array[1]
                 event_map_path = deepcopy(batch["event_map_path"][i])
                 coords = deepcopy(batch["coords"][i])
                 record = {"pandda_name": pandda_name,
                           "dtag": dtag,
                           "event_idx": event_idx,
                           "true_class": true_class,
-                          "estimated_class": estimated_class,
                           "event_map_path": event_map_path,
                           "coords": coords,
                           }
@@ -206,13 +126,10 @@ def train(network,
 
             del id_batch
             del label_batch
-            del estimated_label_batch
             del batch
 
 
-        # except Exception as e:
-        #     print("Failed for some reason")
-        #     print(e)
+     
         training_table = pd.DataFrame(labels)
         print(training_table.head())
 
