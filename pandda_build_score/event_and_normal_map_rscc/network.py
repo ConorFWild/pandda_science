7d1a88d29c27ce8c43b061c870fe7a00a58555d5
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2020-04-28 11:00:58 +0100

    Update network.py

--- pandda_build_score/event_and_normal_map_rscc/network.py
+++ pandda_build_score/event_and_normal_map_rscc/network.py
@@ -416,7 +416,7 @@ class SqueezeNet(nn.Module):
             nn.Dropout(p=0.5),
             final_conv,
             nn.ReLU(inplace=True),
-            nn.AdaptiveAvgPool3d((1, 1))
+            nn.AdaptiveAvgPool3d((1, 1, 1))
         )
 
         for m in self.modules():
