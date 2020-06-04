5b37d902a4c6903458c82a0a70df6953180a3f79
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2020-06-04 19:22:49 +0100

    Update get_autobuilding_comparison_set_coot.py

--- pandda_autobuilding/get_autobuilding_comparison_set_coot.py
+++ pandda_autobuilding/get_autobuilding_comparison_set_coot.py
@@ -278,14 +278,15 @@ def select_event(events, rsccs):
     print("getting event!")
     high_rscc_event_keys = list(filter(lambda x: rsccs[x] > 0.7,
                                        rsccs))
+    print("High rscc keys: {}".format(high_rscc_event_keys[0]))
     actually_built_high_rscc_event_keys = list(filter(lambda x: events[x].actually_built == True,
                                                       high_rscc_event_keys))
-    print("RSCC keys actually built: {}".format(actually_built_high_rscc_event_keys))
+    print("RSCC keys actually built: {}".format(actually_built_high_rscc_event_keys[0]))
 
     events_near_models = list(filter(lambda x: events[x].distance_to_ligand_model < 10.0,
                                      actually_built_high_rscc_event_keys,
                                      ))
-    print("Events near model: {}".format(events_near_models))
+    print("Events near model: {}".format(events_near_models[0]))
 
     event_key = choose_one(events_near_models)
     print(list(events.keys())[0])
