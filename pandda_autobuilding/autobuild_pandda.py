a05088926804b07a779fc660a9ac4644b9fc84a9
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2020-06-04 11:40:42 +0100

    Update autobuild_pandda.py

--- pandda_autobuilding/autobuild_pandda.py
+++ pandda_autobuilding/autobuild_pandda.py
@@ -136,7 +136,7 @@ def elbow(autobuilding_dir, ligand_smiles_path):
     command = "module load phenix; cd {autobuilding_dir}; phenix.elbow {ligand_smiles_path} --output=\"{out}\""
     formatted_command = command.format(autobuilding_dir=autobuilding_dir,
                                        ligand_smiles_path=ligand_smiles_path,
-                                       out="ligand",
+                                       out="autobuilding_ligand",
                                        )
 
     p = subprocess.Popen(formatted_command,
