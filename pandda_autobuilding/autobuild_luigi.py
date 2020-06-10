e7495aa04e569067615ba87ca58ad256f56539ce
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2020-06-10 16:45:58 +0100

    Update autobuild_luigi.py

--- pandda_autobuilding/autobuild_luigi.py
+++ pandda_autobuilding/autobuild_luigi.py
@@ -248,7 +248,7 @@ def get_events(path):
     event_table = pd.read_csv(str(path))
     for idx, event_row in event_table.iterrows():
         if event_row["actually_built"] is True:
-            if Path(event_row["event_map_path"]).exists()
+            if Path(event_row["event_map_path"]).exists():
                 event_row["ligand_smiles_path"] = get_ligand_smiles(Path(event_row["event_map_path"]).parent)
                 event = Event.from_record(event_row)
                 events.append(event)
