478d735e1ec827f269f0dbf7417c2277e41f66eb
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2020-07-15 19:54:07 +0100

    Create fragalysis.py

---
+++ pandda_autobuilding/fragalysis.py
@@ -0,0 +1,20 @@
+from data import GetMoleculesData
+
+
+
+search = GetMoleculesData()
+print(search)
+
+id_list = search.get_target_ids(target='NUDT5A')
+print(id_list)
+
+url = search.set_molecule_url(target_id=id_list[0])
+print(url)
+
+
+# results = get_molecules_json(url=url)
+# print(results)
+
+
+results_table = search.get_all_mol_responses()
+print(results_table)
