0294f60431a82d5a86b6dd1cc84758d3c986ec2a
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2020-07-15 19:55:22 +0100

    Update fragalysis.py

--- pandda_autobuilding/fragalysis.py
+++ pandda_autobuilding/fragalysis.py
@@ -1,20 +1,23 @@
 from data import GetMoleculesData
 
 
+def main():
+    search = GetMoleculesData()
+    print(search)
 
-search = GetMoleculesData()
-print(search)
+    id_list = search.get_target_ids(target='NUDT5A')
+    print(id_list)
 
-id_list = search.get_target_ids(target='NUDT5A')
-print(id_list)
-
-url = search.set_molecule_url(target_id=id_list[0])
-print(url)
-
-
-# results = get_molecules_json(url=url)
-# print(results)
+    url = search.set_molecule_url(target_id=id_list[0])
+    print(url)
 
 
-results_table = search.get_all_mol_responses()
-print(results_table)
+    # results = get_molecules_json(url=url)
+    # print(results)
+
+
+    results_table = search.get_all_mol_responses()
+    print(results_table)
+
+if __name__ == "__main__":
+    
