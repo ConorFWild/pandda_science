7bc83e59588499690b8fcb9e7bbc0bcbcb988a87
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2020-07-20 15:02:47 +0100

    Update fragalysis.py

--- pandda_autobuilding/fragalysis.py
+++ pandda_autobuilding/fragalysis.py
@@ -5,6 +5,8 @@ from fragalysis_api.xcextracter.xcextracter import xcextracter
 def main():
     mpro_summary = xcextracter('Mpro')
     print(mpro_summary)
+    for col in mpro_summary.columns:
+        print(col)
 
 
 if __name__ == "__main__":
