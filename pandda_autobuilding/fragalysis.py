60493b5d6330d02da16b07efebcb10e8f41c91a2
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2020-07-22 17:43:05 +0100

    Update fragalysis.py

--- pandda_autobuilding/fragalysis.py
+++ pandda_autobuilding/fragalysis.py
@@ -124,6 +124,7 @@ def get_reference_models(project_code):
 
     for index, row in summary.iterrows():
         protein_code = row["protein_code"]
+        print(protein_code)
         dtag = protein_code_to_dtag(protein_code)
         print(dtag)
         pdb_block = pdb_grabber.get_bound_pdb_file(protein_code)
