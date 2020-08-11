10fd39f26e1ad96cfdd1c43a13ada1ddd327f738
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2020-08-11 16:53:24 +0100

    Update autobuild_pandda.py

--- pandda_autobuilding/autobuild_pandda.py
+++ pandda_autobuilding/autobuild_pandda.py
@@ -479,7 +479,7 @@ class PanDDAFilesystemModel:
         self.pandda_root_dir = pandda_root_dir
 
         self.pandda_analyse_dir = pandda_root_dir / "analyses"
-        self.pandda_inspect_events_path = self.pandda_analyse_dir / "pandda_inspect_events.csv"
+        self.pandda_inspect_events_path = self.pandda_analyse_dir / "pandda_analyse_events.csv"
         self.autobuilding_results_table = self.pandda_analyse_dir / "autobuilding_results.csv"
 
         self.pandda_processed_datasets_dir = pandda_root_dir / "processed_datasets"
