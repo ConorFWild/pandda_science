34de7f4deb635611f51fdb7ca4d28c8ced7dc214
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2020-08-24 14:04:04 +0100

    Update map2sf.py

--- autobuilding_paper/map2sf.py
+++ autobuilding_paper/map2sf.py
@@ -40,13 +40,15 @@ def main():
     config = Config.from_config()
 
     m = gemmi.read_ccp4_map(str(config.xmap_in))
+    print(m.grid.spacegroup)
     # m.setup()
     sf = gemmi.transform_map_to_f_phi(m.grid, half_l=True)
+    print(sf.spacegroup)
     # data = sf
     print(dir(sf))
     data = sf.prepare_asu_data()
 
-    mtz = gemmi.Mtz(with_base=True)
+    mtz = gemmi.Mtz(with_base=True, dmin=2.0)
     # mtz = gemmi.Mtz()
     print(dir(mtz))
     mtz.spacegroup = sf.spacegroup
