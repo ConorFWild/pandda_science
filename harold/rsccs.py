3e449afd75148c9192a83d5660c0d4b4f3f388d8
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2020-09-11 17:45:22 +0100

    harold

---
+++ harold/rsccs.py
@@ -0,0 +1,259 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+import typing
+
+import re
+from pathlib import Path
+from subprocess import Popen, PIPE
+
+import pandas as pd
+
+import gemmi
+
+# constants
+RSCC_TABLE_FILE = "/dls/labxchem/data/2019/lb18145-130/processing/analysis/RSCC/table.csv"
+MODELS_DIR = "/dls/labxchem/data/2019/lb18145-130/processing/analysis/RSCC"
+MAPS_DIR = "/dls/labxchem/data/2019/lb18145-130/processing/analysis/model_building"
+EVENT_MAP_FILE = "{dtag}-event_{event_idx}_1-BDC_{}_map.native.ccp4"
+
+FOFC_MAP_PATTERN = "fofc.map"
+TWOFOFC_MAP_PATTERN = "2fofc.map"
+EVENT_MAP_PATTERN = "[^\-]+\-[^\-]+\-event_[0-9]+_1\-BDC_[^_]+_map.native.ccp4"
+
+PHENIX_ENV = "module load phenix"
+PHENIX_MODEL_VS_MAP = "phenix.model_vs_map"
+PHENIX_MAP_MODEL_CC = "phenix.map_model_cc"
+CC_PER_RESIDUE_LOG_FILE = "cc_per_residue.log"
+CC_PER_RESIDUE_LOG_RSCC_PATTERN = "[a-zA-Z]+\s+LIG\s+[0-9]+\s([^\s]+)"
+
+
+@dataclass()
+class Dtag:
+    dtag: str
+
+    @staticmethod
+    def from_rscc_path(path: Path):
+        pattern = "RSCC_([^\.]+\.pdb)"
+        string = str(path)
+        dtag = re.match(pattern,
+                        string,
+                        ).group(0)
+        return Dtag(dtag)
+
+
+@dataclass()
+class Models:
+    models: typing.Dict[Dtag, Path]
+
+    def __iter__(self):
+        for dtag in self.models:
+            yield dtag
+
+    def __getitem__(self, item):
+        return self.models[item]
+
+    @staticmethod
+    def from_dir(directory: Path):
+        models = {}
+        for model_path in directory.glob("*"):
+            dtag = Dtag.from_rscc_path(model_path)
+            models[dtag] = model_path
+
+        return Models(models)
+
+
+@dataclass()
+class Maps:
+    maps: typing.Dict[Dtag, Path]
+
+    def __iter__(self):
+        for dtag in self.maps:
+            yield dtag
+
+    def __getitem__(self, item):
+        return self.maps[item]
+
+    @staticmethod
+    def from_dir(directory: Path, pattern: str):
+
+        maps = {}
+        for dataset_path in directory.glob("*"):
+            dtag = Dtag(dataset_path.name)
+            for dataset_dir_file in dataset_path.glob("*"):
+                if re.match(pattern, str(dataset_dir_file)):
+                    xmap_file = dataset_dir_file
+                    maps[dtag] = xmap_file
+                    continue
+
+        return Maps(maps)
+
+
+@dataclass()
+class Model:
+    pass
+
+
+@dataclass()
+class Map:
+    pass
+
+
+@dataclass()
+class RSCC:
+    rscc: float
+
+    @staticmethod
+    def from_model_and_map(model: Path, xmap: Path, resolution: Resolution):
+        command = f"{PHENIX_ENV}; {PHENIX_MAP_MODEL_CC} {model} {xmap} resolution={resolution} cc_per_residue=True"
+
+        process = Popen(command,
+                        shell=True,
+                        stdout=PIPE,
+                        )
+
+        stdout, stderr = process.communicate()
+
+        rscc = RSCC.parse_log()
+
+        return RSCC(rscc)
+
+    @staticmethod
+    def parse_stdout(stdout: str):
+        with open(CC_PER_RESIDUE_LOG_FILE, "r") as f:
+            string = f.read()
+
+        matches = re.findall(CC_PER_RESIDUE_LOG_RSCC_PATTERN,
+                             string)
+
+        first_match = next(matches)
+        first_match_capture_group = first_match.group(1)
+        rscc = float(first_match_capture_group)
+
+        return rscc
+
+    @staticmethod
+    def parse_log():
+        with open(CC_PER_RESIDUE_LOG_FILE, "r") as f:
+            string = f.read()
+
+        matches = re.findall(CC_PER_RESIDUE_LOG_RSCC_PATTERN,
+                             string)
+
+        first_match = next(matches)
+        first_match_capture_group = first_match.group(1)
+        rscc = float(first_match_capture_group)
+
+        return rscc
+
+
+@dataclass()
+class RSCCs:
+    rsccs: typing.Dict[Dtag, RSCC]
+
+    @staticmethod
+    def from_models_and_maps(models: Models, maps: Maps, mtzs: MTZs):
+        rsccs: typing.Dict[Dtag, RSCC] = {}
+        for dtag in models:
+            model = models[dtag]
+            xmap = maps[dtag]
+            mtz = mtzs[dtag]
+            resolution = Resolution.from_mtz(mtz)
+            rsccs[dtag] = RSCC.from_model_and_map(model,
+                                                  xmap,
+                                                  resolution,
+                                                  )
+
+        return RSCCs(rsccs)
+
+    def to_table(self):
+        records = []
+        for dtag in self.rsccs:
+            record = {"dtag": dtag.dtag,
+                      "rscc": self.rsccs[dtag].rscc,
+                      }
+            records.append(record)
+
+        return pd.DataFrame(records)
+
+
+@dataclass()
+class MTZs:
+    mtzs: typing.Dict[Dtag, Path]
+
+    @staticmethod
+    def from_dir(pandda_dir: Path, mtz_pattern: str):
+        mtzs = {}
+        for dataset_dir in pandda_dir.glob("*"):
+            dtag = Dtag(dataset_dir.name)
+            dataset_dir_files = dataset_dir.glob("*")
+            for dataset_dir_file in dataset_dir_files:
+                if re.match(mtz_pattern, str(dataset_dir_file)):
+                    mtz_file = dataset_dir_file
+                    mtzs[dtag] = mtz_file
+                    continue
+
+        return MTZs(mtzs)
+
+    def __getitem__(self, item):
+        return self.mtzs[item]
+
+
+@dataclass()
+class Resolution:
+    resolution: float
+
+    @staticmethod
+    def from_mtz(mtz_file: Path):
+        mtz = gemmi.read_mtz_file(str(mtz_file))
+        resolution = mtz.resolution_high()
+
+        return Resolution(resolution)
+
+
+def main():
+    models: Models = Models.from_dir(Path(MODELS_DIR))
+    mtzs: MTZs = MTZs.from_dir(Path(MAPS_DIR),
+                               EVENT_MAP_PATTERN,
+                               )
+
+    # Event maps
+    maps: Maps = Maps.from_dir(Path(MAPS_DIR),
+                               EVENT_MAP_PATTERN,
+                               )
+    rsccs_event_maps = RSCCs.from_models_and_maps(models,
+                                                  maps,
+                                                  mtzs,
+                                                  )
+    rsccs_event_maps_table = rsccs_event_maps.to_table()
+    rsccs_event_maps_table["type"] = "event_map"
+
+    # FOFC maps
+    maps: Maps = Maps.from_dir(Path(MAPS_DIR),
+                               FOFC_MAP_PATTERN,
+                               )
+    rsccs_event_maps = RSCCs.from_models_and_maps(models,
+                                                  maps,
+                                                  mtzs,
+                                                  )
+    rsccs_fofc_table = rsccs_event_maps.to_table()
+    rsccs_fofc_table["type"] = "FOFC"
+
+    # 2FOFC maps
+    maps: Maps = Maps.from_dir(Path(MAPS_DIR),
+                               TWOFOFC_MAP_PATTERN,
+                               )
+    rsccs_event_maps = RSCCs.from_models_and_maps(models,
+                                                  maps,
+                                                  mtzs,
+                                                  )
+    rsccs_2fofc_table = rsccs_event_maps.to_table()
+    rsccs_2fofc_table["type"] = "2FOFC"
+
+    # output
+    table = pd.concat([rsccs_event_maps_table, rsccs_fofc_table, rsccs_2fofc_table])
+    table.to_csv(str(RSCC_TABLE_FILE))
+
+
+if __name__ == "__main__":
+    main()
