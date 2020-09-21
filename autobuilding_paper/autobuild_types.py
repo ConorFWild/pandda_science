6b80cb80728e19ba0a94e29a7efa8e0d192ca4d2
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2020-09-21 19:28:20 +0100

    Update autobuild_types.py

--- autobuilding_paper/autobuild_types.py
+++ autobuilding_paper/autobuild_types.py
@@ -283,7 +283,10 @@ class Structure:
                         if pos.dist(event_centoid) > radius:
                             new_structure[model_i][chain_i][residue_i].add_atom(atom, pos=-1)
 
-        return Structure(new_structure)
+        for model_i, model in self.structure:
+            self.structure[model_i] = new_structure[model_i]
+
+        return Structure(self.structure)
 
 
 @dataclass()
